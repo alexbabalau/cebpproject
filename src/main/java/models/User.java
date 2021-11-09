1e22a9e3de84d3b61a76413a8dd67556c79bd376
alexbabalau <alex.babalau@gmail.com>
2021-11-09 04:26:35 +0200

    Added amount field to user model

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -3,11 +3,21 @@ package models;
 public class User extends VersionedEntity{
     private Integer id;
     private String username;
+    private Double amount;
 
-    public User(Integer version, Integer id, String username) {
+    public User(Integer version, Integer id, String username, Double amount) {
         super(version);
         this.id = id;
         this.username = username;
+        this.amount = amount;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
     }
 
     public Integer getId() {
